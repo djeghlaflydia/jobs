1e5457bdf65c2849c972efdb5c150f2268d598fc
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-10-14 14:21:11 +0100

    fixed name compo

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
-import JobsViews from '@/views/JobsViews.vue';
+import JobsViews from '@/views/JobsView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
-import JobsView from '@/views/JobsView.vue';
+import JobsView from '@/views/JobView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
