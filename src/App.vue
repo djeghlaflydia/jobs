cce87ebe1e686e8e33a067e380afd77bf944c621
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-10-14 12:22:53 +0100

    add new components and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
 </script>
 
 
 <template>
   <Navbar/>
-  <Hero/>
+ <!--<Hero title="Test titre" subtitle = "test subtitle"/>--> 
+ <Hero/>
+ <HomeCards/>
+ <JobListings/>
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,27 @@
+<script setup>
+import JobListing from "@/components/JobListing.vue";
+import JobData from "@/jobs copy.json";
+import {ref} from 'vue';
+
+const jobs = ref(JobData);
+//console.log(jobs.value);
+
+</script>
+
+<template>
+
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl front-bold text-green-500 mb-6 text-center">
+                Brrows jobs
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in jobs" :key="job.id"    :job="job"/>
+            </div>
+
+        </div>
+    </section>
+
+    
+
+</template>
